faa737fbe2a4891c64c20d5797e77f53b0f0fedf
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-15 12:17:44 +0300

    Modify shorterApp with our own ioc
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/shorter/ShorterApp.java
+++ src/main/java/shorter/ShorterApp.java
@@ -1,5 +1,7 @@
 package shorter;
 
+import ioc.BeanFactory;
+import ioc.JavaConfAppContext;
 import shorter.model.Link;
 import shorter.repository.InMemShortLinksRepo;
 import shorter.repository.ShortLinksRepo;
@@ -8,6 +10,8 @@ import shorter.service.IdentShorterService;
 import shorter.service.ShortenLinkService;
 import shorter.service.ShorterService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static shorter.model.Link.linkTo;
@@ -17,8 +21,17 @@ public class ShorterApp {
     public static void main(String[] args) {
         String url = "https://www.facebook.com/groups/KyivKUG/";
 
-        ShortLinksRepo inMemShortLinksRepo = new InMemShortLinksRepo();
+        /*`ShortLinksRepo inMemShortLinksRepo = new InMemShortLinksRepo();
         ShorterService shorterService = new IdentShorterService();
+        */
+        Map<String, Class<?>> config = new HashMap<>() {{
+            put("shorterService", IdentShorterService.class);
+            put("linksRepo", InMemShortLinksRepo.class);
+        }};
+        BeanFactory context = new JavaConfAppContext(config);
+        ShorterService shorterService = context.getBean("shorterService");
+        ShortLinksRepo inMemShortLinksRepo = context.getBean("linksRepo");
+
         ShortenLinkService shortenLinkService = new DefaultShortenLinkService(inMemShortLinksRepo, shorterService);
         Link shortLink = shortenLinkService.shortLink(linkTo(url));
         System.out.println("Short link: " + shortLink.link());
