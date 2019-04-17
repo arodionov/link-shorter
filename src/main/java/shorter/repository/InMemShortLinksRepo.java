dad51d53013fab46a6a9f8607f54d9bded4915c9
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-17 23:10:55 +0300

    Modify InMemShortLinksRepo
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/shorter/repository/InMemShortLinksRepo.java
+++ src/main/java/shorter/repository/InMemShortLinksRepo.java
@@ -1,38 +1,21 @@
 package shorter.repository;
 
-import ioc.annotations.Benchmark;
-import ioc.annotations.PostConstructBean;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class InMemShortLinksRepo implements ShortLinksRepo {
 
-	private Map<String, String> links = new HashMap<>();
+    private Map<String, String> links = new HashMap<>();
 
-	@Override
-	public Optional<String> get(String shortPath) {
-		return Optional.ofNullable(links.get(shortPath));
-	}
+    @Override
+    public Optional<String> get(String shortPath) {
+        return Optional.ofNullable(links.get(shortPath));
+    }
 
-	@Override
-	public void put(String shortPath, String fullPath) {
-		links.put(shortPath, fullPath);
-	}
-
-	public void init() {
-		System.out.println("Hello");
-	}
-
-	@PostConstructBean
-	public void postConstruct() {
-		System.out.println("World");
-	}
-
-	@Benchmark
-	public void measuredMethod() {
-        System.out.println("!");
+    @Override
+    public void put(String shortPath, String fullPath) {
+        links.put(shortPath, fullPath);
     }
 
 }
