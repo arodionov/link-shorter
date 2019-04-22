c075711fbf5600f8c5f2418445f1532130498f9c
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-22 22:11:41 +0300

    Add AppConfig class
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/shorter/AppConfig.java
@@ -0,0 +1,10 @@
+package shorter;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    //TODO: configuration for business logic - services, repos, components
+
+}
--- src/main/java/shorter/WebConfig.java
+++ src/main/java/shorter/WebConfig.java
@@ -13,4 +13,9 @@ public class WebConfig {
         return new HelloController();
     }
 
+    //TODO: create ShorterController bean to make short link from full
+    //req examp: http://localhost:8080/shorter/short?full=http://jug.ua
+
+    //TODO: create FullController bean to make full link from short
+    //req examp: http://localhost:8080/shorter/full?short=http://asdsa.sh
 }
