e1c49b426ad8f1dcd06171969b35bee5006e0cfe
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-22 22:07:22 +0300

    Add MyContextLoaderListener class
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/web/MyContextLoaderListener.java
@@ -0,0 +1,17 @@
+package web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyContextLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(final ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(final ServletContextEvent servletContextEvent) {
+
+    }
+}
