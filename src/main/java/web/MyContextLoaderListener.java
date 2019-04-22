e08bf6191e22711436c308f9bf02add583934b03
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-23 00:48:57 +0300

    Remove printlns
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/web/MyContextLoaderListener.java
+++ src/main/java/web/MyContextLoaderListener.java
@@ -9,14 +9,11 @@ public class MyContextLoaderListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(final ServletContextEvent servletContextEvent) {
-        System.out.println("QWERTYUI");
-        System.out.println(servletContextEvent.getServletContext().getClass().getName());
 
     }
 
     @Override
     public void contextDestroyed(final ServletContextEvent servletContextEvent) {
-        System.out.println("QWERTYUI1");
 
     }
 }
