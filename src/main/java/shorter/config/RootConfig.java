a73068f4511287258b13a41849f9cfe39ebf0913
D_Garbar <DimaGarbar49@gmail.com>
2019-04-23 18:51:04 +0300

    Fix a small bug. @Configuration is annotated with @Component
    So RootConfig Beans need to exclude by annotation @Controller AND @Configuration

--- src/main/java/shorter/config/RootConfig.java
+++ src/main/java/shorter/config/RootConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 @ComponentScan(basePackages = "shorter", excludeFilters = {
-	@Filter(pattern = "shorter\\.controller.*", type = FilterType.REGEX),
+	@Filter(Controller.class),
+	//Configuration is annotated with Component LOLOLOLOLOLOLOL
+	@Filter(Configuration.class)
 })
 public class RootConfig {
 
